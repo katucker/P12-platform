374dfa0aabf615621c93de37d4e046cb1b5a5779
Keith Tucker <tuck220@icloud.com>
2022-03-07 09:01:58 -0500

    ED style and initial D3
    Switch to ED style guide and added initial D3 code for visualizations.

--- src/p12-platform.py
+++ src/p12-platform.py
@@ -5,30 +5,38 @@ Created on Sun Feb 27 12:13:47 2022
 @author: ktucker
 """
 import os
+import time
 from flask import Flask, render_template, redirect, request
+from pathlib import Path
 
 app = Flask(__name__)
 
+BASE_DIR = Path(__file__).parent
 
+def template_modified(template):
+	filepath = BASE_DIR.joinpath('templates',template)
+	mtime = time.localtime(filepath.stat().st_mtime) 
+	return time.strftime('%B %d, %Y', mtime)
+	
 @app.route('/')
 def show_alternatives():
-    return render_template('index.html')
+    return render_template('index.html', mtime=template_modified('index.html'))
     
 @app.route('/show-tell')
 def show_and_tell():
-	return render_template('show-tell.html')
+	return render_template('show-tell.html', mtime=template_modified('show-tell.html'))
 
 @app.route('/helpachild')
 def helpachild():
-	return render_template('helpachild.html')
+	return render_template('helpachild.html', mtime=template_modified('helpachild.html'))
 
 @app.route('/p12equity')
 def p12equity():
-	return render_template('p12equity.html')
+	return render_template('p12equity.html', mtime=template_modified('p12equity.html'))
 
 @app.route('/get_stats', methods=['POST'])
 def get_stats():
-	return f"Age {request.form.get('age')}<br>Grade {request.form.get('grade')}<br>Race {request.form.get('race')}<br>Hispanic/Latino {request.form.get('hispanic')}<br>Disability {request.form.get('disability')}<br>Economically Disadvantaged {request.form.get('economic')}<br>Experiencing Homelessness {request.form.get('homeless')}"
+	return render_template('childviz.html', mtime=template_modified('childviz.html'), age=request.form.get('age'), grade=request.form.get('grade'),race=request.form.get('race'),hispanic=request.form.get('hispanic'),disability=request.form.get('disability'),economic=request.form.get('economic'),homeless=request.form.get('homeless'))
 	
 
 @app.route('/get_area_stats', methods=['POST'])
